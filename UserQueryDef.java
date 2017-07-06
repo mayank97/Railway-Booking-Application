868d09275f414ad0db2f8aaa1566896a9d28b226
mayank97 <mayank.mk18@gmail.com>
2017-07-06 11:33:32 +0530

    Add files via upload

---
+++ UserQueryDef.java
@@ -0,0 +1,18 @@
+package DBMS;
+
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class UserQueryDef {
+	
+	UserQueryDef(){
+		test.panel.removeAll();
+		test.panel.repaint();
+		test.panel.revalidate();
+		JLabel lblRailway = new JLabel("RAILWAY");
+		lblRailway.setBounds(207, 12, 258, 244);
+		test.panel.add(lblRailway);
+	}
+	
+
+}
